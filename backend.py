8734cd136b84470171a8358ae2375e3cb5aac3f4
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2024-02-11 19:36:25 -0500

    t

--- backend.py
+++ backend.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-#from flask_restful import Resource, Api
 
 app = Flask(__name__)
-#api = Api(app)
 
 # Mock database with questions and answers
 questions = {
